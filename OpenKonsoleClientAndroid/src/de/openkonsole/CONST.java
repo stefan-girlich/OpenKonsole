ec4998b9e59b780bd962d135d2b5f2f646f28544
JG <jgliwa2@googlemail.com>
2014-03-16 02:59:18 +0100

    erweitert um Konstanten für Befehlsübermittlung

--- OpenKonsoleClientAndroid/src/de/openkonsole/CONST.java
+++ OpenKonsoleClientAndroid/src/de/openkonsole/CONST.java
@@ -1,9 +1,19 @@
 package de.openkonsole;
 
 public class CONST {
-	
+
 	public final static String HOST = "192.168.178.30";
-	
+
 	public final static int PORT = 1337;
+
+	public final static byte BUTTON_START = 0;
+	public final static byte BUTTON_A = 1;
+	public final static byte BUTTON_B = 2;
+	public final static byte BUTTON_C = 3;
+	public final static byte BUTTON_D = 4;
+	public final static byte ANALOG_INPUT = 5;
+
+	public final static byte BUTTON_DOWN = 0;
+	public final static byte BUTTON_UP = 1;
 
 }
