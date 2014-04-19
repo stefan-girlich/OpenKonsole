2a38b56d25e0205b94bd478fe8625bc128fc8b9d
JG <jgliwa2@googlemail.com>
2014-04-19 21:47:19 +0200

    Dynamically get HOST IP from OS

--- OpenKonsoleServer/main.js
+++ OpenKonsoleServer/main.js
@@ -1,19 +1,40 @@
 var srv = require('./server.js');
+var dns = require('dns');
+var os = require('os');
 
-
-var HOST = '192.168.178.56';	// TODO determine this server's IP dynamically
+var HOST	// TODO determine this server's IP dynamically
 var TCP_PORT = 1337;
 var UDP_PORT = 30300;
 
-// TODO constructor args vs .listen() args
-var broadcastSrv = new srv.BroadcastServer(HOST, UDP_PORT, 3000, TCP_PORT);
-broadcastSrv.start();
+// First resolve the Host IP, then start the server.
+// family returns an integer (4 = IPv4, 6 = IPv6, null = both)
+// TODO Ensure IPv6 compatibility
+dns.lookup(os.hostname(), function (error, address, family) {
+	console.log("HOST resolved to: "+ address);
+	HOST = address;
+	startServer();
+});
 
-var playerSrv = new srv.PlayerServer();
-playerSrv.listen(TCP_PORT, HOST);
+var broadcastSrv;
+var playerSrv;
 
 // seems to be visible in node-webkit; better hide and provide API method
-var players = playerSrv.getPlayers();
+var players;
+
+
+
+function startServer(){
+
+// TODO constructor args vs .listen() args
+broadcastSrv = new srv.BroadcastServer(HOST, UDP_PORT, 3000, TCP_PORT);
+broadcastSrv.start();
+console.log("BroadcastServer started");
+
+playerSrv = new srv.PlayerServer();
+playerSrv.listen(TCP_PORT, HOST);
+// seems to be visible in node-webkit; better hide and provide API method
+players = playerSrv.getPlayers();
+};
 
 
 // TODO what other signals to catch?
