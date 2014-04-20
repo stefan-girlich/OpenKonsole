8e42a082144789e9dbeb57a5936f5caacc2e809d
JG <jgliwa2@googlemail.com>
2014-04-20 19:26:08 +0200

    Added Jade templating + Grunt Task Added Bower Dependency Management + Grunt Task Added Grunt Build for node-wekit Refactor pong application FINALLY GOT IT TO WORK!

--- OpenKonsoleServer/Gruntfile.js
+++ OpenKonsoleServer/Gruntfile.js
@@ -2,35 +2,52 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
+    pkg: grunt.file.readJSON('./pongApp/package.json'),
+	jade:{
+		compile:{
+			files:{
+				"./pongApp/html/index.htm": ["./pongApp/views/index.jade"]
+			},
+			options:{
+				pretty: true
+			}
+		}
+	},
+    clean: {
+		options: { force: true },
+		releases: ["./releases"],
+		jadeCompilations: ["./pongApp/html"]
+	},
+    nodewebkit: {
+		options: {
+			build_dir: './', // Where the build version of my node-webkit app is saved
+			mac: true, // We want to build it for mac
+			win: true, // We want to build it for win
+			linux32: false, // We don't need linux32
+			linux64: false // We don't need linux64
+		},
+		src: ['./pongApp/**'] // Your node-webkit app
+	},
+	bower: {
+		install: {
+			options: {
+				 targetDir: './pongApp/lib',
+				 install: true,
+				 cleanBowerDir: false,
+				 cleanTargetDir: false
+			}
+		}
+  }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-bower-task');
+  
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-node-webkit-builder');
 
-  grunt.initConfig({
-  nodewebkit: {
-    options: {
-        build_dir: './', // Where the build version of my node-webkit app is saved
-        mac: true, // We want to build it for mac
-        win: true, // We want to build it for win
-        linux32: false, // We don't need linux32
-        linux64: false // We don't need linux64
-    },
-    src: ['./*'] // Your node-webkit app
-  },
-})
-
   // Default task(s).
-  grunt.registerTask('default', ['nodewebkit']);
+  grunt.registerTask('default', ['clean','bower', 'jade', 'nodewebkit']);
 
 };
---
+++ OpenKonsoleServer/pongApp/js/main.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+var dns = require('dns');
+var os = require('os');
+eval(fs.readFileSync('./js/server.js')+'');
+
+var HOST	// TODO determine this server's IP dynamically
+var TCP_PORT = 1337;
+var UDP_PORT = 30300;
+
+// First resolve the Host IP, then start the server.
+// family returns an integer (4 = IPv4, 6 = IPv6, null = both)
+// TODO Ensure IPv6 compatibility
+dns.lookup(os.hostname(), function (error, address, family) {
+	console.log("HOST resolved to: "+ address);
+	HOST = address;
+	startServer();
+});
+
+var broadcastSrv;
+var playerSrv = new PlayerServer();
+var ipSniffer;
+
+// seems to be visible in node-webkit; better hide and provide API method
+var players = playerSrv.getPlayers();
+
+
+
+function startServer(){
+
+	console.log("IP sniffer started");	
+	ipSniffer = new IpSniffer(HOST, UDP_PORT, 3000, TCP_PORT);
+	ipSniffer.start();
+
+	// broadcastSrv = new srv.BroadcastServer(HOST, UDP_PORT, 3000, TCP_PORT);
+	// broadcastSrv.start();
+	//console.log("BroadcastServer started");
+
+	console.log("PlayerServer started!");
+	playerSrv.listen(TCP_PORT, HOST);
+};
+
+// TODO what other signals to catch?
+process.on('SIGINT', function() {
+	console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
+	broadcastSrv.stop();
+	playerSrv.stop();
+	ipSniffer.stop();
+	process.exit();
+});
