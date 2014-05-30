7abe703f2f5253b777223572e11afe7989db6f17
Stefan Girlich <stefan.girlich@gmail.com>
2014-05-30 02:42:13 +0200

    re-enable Mac OSX builds

--- OpenKonsoleServer/Gruntfile.js
+++ OpenKonsoleServer/Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function(grunt) {
     nodewebkit: {
 		options: {
 			build_dir: './', // Where the build version of my node-webkit app is saved
-			mac: false, // We want to build it for mac
+			mac: true, // We want to build it for mac
 			win: true, // We want to build it for win
 			linux32: false, // We don't need linux32
 			linux64: false, // We don't need linux64
